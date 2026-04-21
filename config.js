1f0fba1ce733ba17b793207eae9beaa0cdfda462
syakirafachid <syakirafachid@gmail.com>
2026-04-21 15:50:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 //  Get these from: supabase.com → your project → Settings → API
 // ============================================================
 
-const SUPABASE_URL = 'https://jhfkkpakapekyjsvcplx.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://jhfkkpakapekyjsvcplx.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_0uERWOBlwmDIe0Y3271kYQ_kYuHZKU3'
